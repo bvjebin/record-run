7e45067302d4ba576a5204bf2f0e982ce615f02b
Jebin <jebin@datarpm.com>
2014-08-03 20:24:51 +0530

    removed unused method

--- record-run.js
+++ record-run.js
@@ -146,39 +146,6 @@
             return;
         }
 
-        var scrollParent = function (node) {
-            var parents = function (node, ps) {
-                if (node.parentNode === null) { return ps; }
-
-                return parents(node.parentNode, ps.concat([node]));
-            };
-
-            var style = function (node, prop) {
-                return getComputedStyle(node, null).getPropertyValue(prop);
-            };
-
-            var overflow = function (node) {
-                return style(node, "overflow") + style(node, "overflow-y") + style(node, "overflow-x");
-            };
-
-            var scroll = function (node) {
-                return (/(auto|scroll)/).test(overflow(node));
-            };
-
-
-            if (!(node instanceof HTMLElement)) {
-                return ;
-            }
-
-            var ps = parents(node.parentNode, []);
-
-            for (var i = 0; i < ps.length; i += 1) {
-                if (scroll(ps[i])) {
-                    return ps[i];
-                }
-            }
-        };
-
         var runManager = {
 
             counter: 0,
